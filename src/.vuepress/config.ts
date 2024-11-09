f7a85ea03e033139fee62169ecb5f54ec45057e4
szj <846476555@qq.com>
2024-11-10 07:33:29 +0800

    。

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/sjztool/",
 
   lang: "zh-CN",
   title: "By墨気",
